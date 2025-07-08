9c606396a349433d0607486fca5c02dde4f1f10c
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2025-07-08 15:00:45 -0400

    Create not-boring-movies.sql

---
+++ leetcode/SQL 50/not-boring-movies.sql
@@ -0,0 +1,22 @@
+SELECT * FROM cinema WHERE MOD(id,2) != 0 AND description != 'boring' ORDER BY rating DESC;
+
+-- Input: 
+-- Cinema table:
+-- +----+------------+-------------+--------+
+-- | id | movie      | description | rating |
+-- +----+------------+-------------+--------+
+-- | 1  | War        | great 3D    | 8.9    |
+-- | 2  | Science    | fiction     | 8.5    |
+-- | 3  | irish      | boring      | 6.2    |
+-- | 4  | Ice song   | Fantacy     | 8.6    |
+-- | 5  | House card | Interesting | 9.1    |
+-- +----+------------+-------------+--------+
+-- Output: 
+-- +----+------------+-------------+--------+
+-- | id | movie      | description | rating |
+-- +----+------------+-------------+--------+
+-- | 5  | House card | Interesting | 9.1    |
+-- | 1  | War        | great 3D    | 8.9    |
+-- +----+------------+-------------+--------+
+-- Explanation: 
+-- We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
